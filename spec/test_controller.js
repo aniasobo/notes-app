a55f2c15a07c1acde284f3a3abcffecfdea2b8cf
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-07-09 14:44:48 +0100

    Somewhat functional test controller and index.html

--- spec/test_controller.js
+++ spec/test_controller.js
@@ -1,21 +1,29 @@
-'use strict'
+'use strict';
 
-window.addEventListener("load", function() {
-  var submit = document.getElementbyId('submit');
-  var link = document.getElementById('link');
+(function() { 
+  window.addEventListener("load", function() {
 
-  function testController(submit, link, createNote) {
-    this.submit = submit;
-    this.link = link;
-    this.createNote = createNote;
-   };
+  console.log("I HAVE LOADED");
 
-  testController.prototype.submitButton = function() {
-    this.submit.addEventListener('click', function(){
-      var note = document.getElementById('text').innerHTML;
-      this.createNote.createNewNote(note);
-})
+   function TestController() {
+     this.submitButton = document.querySelector("#submit");
+     this.noteForm = document.querySelector("#addnote");
+     this.noteTextInput = document.querySelector("#notetext");
 
+     this.submitButtonAction = function() {
+         this.submitButton.addEventListener("click", function(){
+           console.log("you clicked submit");
+           var note = document.querySelector("#notetext").value;
+           var node = document.createElement("li");
+           var textnode = document.createTextNode(note);
+           node.appendChild(textnode);
+           console.log(note);
+           document.querySelector("#fullnote").appendChild(node);
+         });
+    };
+    this.submitButtonAction();
+  };
+  new TestController();
+});
 
-  }
-})
+})();
